42286a6461a28da2b8ff671d8946c35a3b8ca89c
stephen jones <stephen.jones@alertlogic.com>
2020-03-26 10:28:23 +0000

    Initial checkin for Iris V3 api

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,1 @@
 export * from './old-types';
-
-export interface AlIncidentFilterDictionary {
-    classifications:{[classification:string]:{
-        value:string,
-        caption:string
-    }};
-
-    detectionSources:{[detectionSource:string]:{
-        value:string,
-        caption:string
-    }};
-
-    threatLevels:{[threatLevel:string]:{
-        value:string,
-        caption:string
-    }};
-}
